340dad32b24da7e8999276f3f5af334c10a6f80d
chris-mahmood <chrisjmwolf@gmail.com>
2019-08-15 16:03:36 +0900

    Add CSS, Fix bad game logic

--- app.js
+++ app.js
@@ -3,22 +3,125 @@ let loss = 0;
 let tie = 0;
 
 
-
-
-function computerPlay() {
-    let x = Math.floor(Math.random() * 3) + 1
+let computerPlay = () => { //Computer's Choice
+    let x = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     switch (x) {
         case 1:
             return "rock";
         case 2:
             return "paper";
-
         case 3:
             return "scissors";
     }
 }
 
-let playerPlay = () => {
+let resultTotals = (result) => { //Increments results 
+    if (result === "You Win!") {
+        ++win;
+        totalWins.textContent = (` You have ${win} wins`);
+    }
+    else if (result === "You Lose!") {
+        ++loss;
+        totalLosses.textContent = (` You have ${loss} losses`);
+    }
+    else {
+        ++tie;
+        totalTies.textContent = (` You have ${tie} ties`);
+    }
+
+}
+
+
+
+
+let fadeOut= () =>{
+
+    document.getElementById("content").style.filter = "grayscale(1)";
+    }
+
+let winCondition = () => { //Checks for win conditions and prints the applicable message/image. TODO: Add images, play again btn with refresh. 
+    if (win === 7) {
+        results.textContent = "BY AZURA BY AZURA BY AZURA, IT'S THE GRAND CHAMPION!";
+        fadeOut();
+        }
+    
+    else if (loss === 7) {
+        results.textContent = "Is this loss...?";
+        fadeOut();
+   
+    }
+    else if (tie === 7) {
+        results.textContent = "It's a tie. Nobody likes ties. Have another go at it?";
+    fadeOut();
+    }
+    else { return; } // if none of the game-ending conditions are met, exits  function here
+    showReset();
+}
+
+let showReset = () => {
+    document.getElementById("btnRock").style.display = "none";
+    document.getElementById("btnPaper").style.display = "none";
+    document.getElementById("btnScissors").style.display = "none";
+    document.getElementById("btnReset").style.display = "block";
+}
+
+let togStats = () => {
+    let stats = document.getElementById("stats");
+    if (stats.style.display === "none" || stats.style.display == '') {
+        stats.style.display = "block";
+    } else {
+        stats.style.display = "none";
+    }
+
+}
+
+//Buttons for each player option, stat toggle, and reset button for end of game.
+btnStats.addEventListener('click', function () { togStats() });
+btnRock.addEventListener('click', function () { rockPaperscissors('rock', computerPlay()) });
+btnPaper.addEventListener('click', function () { rockPaperscissors('paper', computerPlay()) });
+btnScissors.addEventListener('click', function () { rockPaperscissors('scissors', computerPlay()) });
+btnReset.addEventListener('click', function () { location.reload(); })
+
+
+
+function rockPaperscissors(playerSelection, computerSelection) {
+    let result = "";
+    if (playerSelection == computerSelection) {
+        result = "TIE!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        result = "You Win!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        result = "You Lose!";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        result = "You Lose!";
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        result = "You Win!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        result = "You Lose!";
+    }
+    else { result = "You Win!"; }
+
+    //output results to main page
+    results.textContent = result;
+    //output total stats to main page  
+    resultTotals(result);
+    //Check For x Wins/Losses/Ties    
+    winCondition();
+
+}
+
+
+
+
+
+
+//Old function for prompting player to manually type choice. 
+/*let playerPlay = () => {
     let choice;
     choice = prompt("Rock, Paper, or Scissors?");
 
@@ -31,67 +134,9 @@ let playerPlay = () => {
     return choice.toLowerCase();
 
 }
-
-function rockPaperscissors(playerSelection, computerSelection) {
-    let result = "";
-
-    if (playerSelection == computerSelection) {
-        result = "TIE!";
-    }
-    else if (playerSelection == "rock", computerSelection == "scissors") {
-        result = "You Win!";
-    }
-    else if (playerSelection == "rock", computerSelection == "paper") {
-        result = "You Lose!";
-    }
-    else if (playerSelection == "paper", computerSelection == "scissors") {
-        result = "You Lose!";
-    }
-    else if (playerSelection == "paper", computerSelection == "rock") {
-        result = "You Win!";
-    }
-    else if (playerSelection == "scissors", computerSelection == "rock") {
-        result = "You Lose!";
-    }
-    else { result = "You Win!"; }
-
-    //output results to main page
-    results.textContent = result;
-    //output total stats to main page  TODO: Make into own function (resultTotals)
-    if (result === "You Win!") {
-        ++win;
-        totalWins.textContent = (result + ` You have ${win} wins`);
-    }
-    else if (result === "You Lose!") {
-        ++loss;
-        totalLosses.textContent = (result + ` You have ${loss} losses`);
-    }
-    else {
-        ++tie;
-        totalTies.textContent = (result + ` You have ${tie} ties`);
-    }
-    //Check For x Wins/Losses/Ties     TODO: Make into own function (winCondition)
-
-    if (win === 7) {
-        results.textContent = "BY AZURA BY AZURA BY AZURA, IT'S THE GRAND CHAMPION!";
-    }
-    else if (loss === 7) {
-        results.textContent = "Is this loss...?";
-    }
-    else if (tie === 7) {
-        results.textContent = "It's a tie. Nobody likes ties. press f5 and do it again.";
-    }
-
-}
-
-
-btnRock.addEventListener('click', function () { rockPaperscissors('rock', computerPlay()) });
-btnPaper.addEventListener('click', function () { rockPaperscissors('paper', computerPlay()) });
-btnScissors.addEventListener('click', function () { rockPaperscissors('scissors', computerPlay()) });
-
-//Old function for original implementation in console.log 
+//Old function for original implementation in console.log
 /*function game() {
-    
+
 
     for (i = 0; i < 5; i++) {
         let computerSelection = computerPlay();
